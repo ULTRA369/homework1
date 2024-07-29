ff9116f7103c43192782b8c23baea20d87efcb3f
Svetlana Linberg <ya.vgb@list.ru>
2024-07-30 00:52:36 +0300

    added project to the repository

---
+++ pythonProject/src/masks.py
@@ -0,0 +1,16 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция, которая возвращает маску карты"""
+    masked_card = card_number[:4] + ' ' + card_number[4:6] + '** ****' + ' ' + card_number[-4:]
+    return masked_card
+
+
+print(get_mask_card_number(input('Введите номер карты: ')))
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция, которая возвращает маску счета"""
+    masked_account_number = '**' + account_number[-4:]
+    return masked_account_number
+
+
+print(get_mask_account(input('Введите номер счета: ')))
