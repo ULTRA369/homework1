b6da1bbca45aad4974a3df45c2f34a1f609e099c
Svetlana Linberg <ya.vgb@list.ru>
2024-07-30 10:22:59 +0300

    init

--- pythonProject/src/widget.py
+++ pythonProject/src/widget.py
@@ -2,6 +2,7 @@ from masks import get_mask_card_number, get_mask_account
 
 card_or_account_request = input('Введите номер карты или счета: ')
 
+
 def mask_account_card(nums: str) -> str:
     """ Функция принимает тип и номер карты или номер счета выводя их замаскированными"""
     if 'Счет' not in nums:
@@ -12,6 +13,7 @@ def mask_account_card(nums: str) -> str:
         account = get_mask_account(nums[-20:])
         new_account = nums.replace(nums[-20:], account)
         return new_account
+
 
 print(mask_account_card(card_or_account_request))
 
