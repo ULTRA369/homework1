641ea98df26f0e0f31aecbb7c501e272773bd1b9
Svetlana Linberg <ya.vgb@list.ru>
2024-08-11 03:25:48 +0300

    updating test_processing.py

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -3,8 +3,13 @@ from typing import Any
 from src.processing import filter_by_state, sort_by_date
 
 
-def test_filter_by_state(by_state: list, expected) -> Any:
-    assert filter_by_state(by_state) == expected
+def test_filter_by_state(by_state):
+    exp = filter_by_state(by_state, "EXECUTED")
+    result = list(exp)
+    assert result == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
 
 
 def test_filter_by_state_canceled(by_state):
