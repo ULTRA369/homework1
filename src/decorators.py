5f48ee0c79407c2ffbef14c2f9f12b04f7d3b11f
Svetlana Linberg <ya.vgb@list.ru>
2024-08-14 07:04:03 +0300

    updating file decorators and test_decorators

--- src/decorators.py
+++ src/decorators.py
@@ -1,48 +1,23 @@
-import logging
-import time
-from functools import wraps
-from typing import Any, Callable, Optional
+def log(filename=""):
+    """Decorator create log about function operation."""
 
-
-def log(filename: Optional[str] = None) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
-    """Декоратор log автоматически логирует начало и конец выполнения функции и результаты или возникшие ошибки"""
-    def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
-        @wraps(func)
-        def wrapper(*args: Any, **kwargs: Any) -> Any:
-            start_time = time.time()
+    def my_decorator(func):
+        def wrapper(*args, **kwargs):
             try:
                 result = func(*args, **kwargs)
-                end_time = time.time()
-                execution_time = end_time - start_time
-                log_message = (
-                    f"{func.__name__} ok. Execution time: {execution_time:.2f} seconds. "
-                    f"Inputs: {args}, Result: {result}"
-                )
                 if filename:
-                    logging.basicConfig(filename=filename, level=logging.INFO)
-                    logging.info(log_message)
+                    with open(filename, "w") as file:
+                        file.write(f"{func.__name__} ok")
                 else:
-                    print(log_message)
+                    print(f"{func.__name__} ok")
                 return result
             except Exception as e:
-                end_time = time.time()
-                execution_time = end_time - start_time
-                log_message = f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}. Error: {str(e)}"
                 if filename:
-                    logging.basicConfig(filename=filename, level=logging.ERROR)
-                    logging.error(log_message)
+                    with open(filename, "w") as file:
+                        file.write(f"{func.__name__} error: {e.__class__.__name__}. Inputs: {args}, {kwargs}")
                 else:
-                    print(log_message)
-                raise
+                    print(f"{func.__name__} error: {e.__class__.__name__}. Inputs: {args}, {kwargs}")
 
         return wrapper
 
-    return decorator
-
-
-@log(filename="mylog.txt")
-def my_function(x: int, y: int) -> int:
-    return x + y
-
-
-my_function(1, 2)
+    return my_decorator
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -5,27 +5,35 @@ import pytest
 from src.decorators import log
 
 
-@log(filename="src\\mylog.txt")
-def my_function(x: int, y: int) -> int:
-    """Сложите два целых числа и верните результат"""
-    return x + y
+def test_log_to_file(tmpdir):
+    log_file = tmpdir.join("test_log.txt")
 
+    # Тестовая функция для успешного выполнения с логированием в файл
+    @log(filename=str(log_file))
+    def test_function_file_success(x, y):
+        return x + y
 
-    @log(filename)
-    def test_func() -> None:
-        """Тест func"""
-        my_function(1, 2)
+    # Тестовая функция для генерации исключения с логированием в файл
+    @log(filename=str(log_file))
+    def test_function_file_error(x, y):
+        raise ValueError("Test error")
 
-    test_func()
+    #Проверка вывода в файл правильную работу функции
+    test_function_file_success(1, 2)
 
-    captured = capsys.readouterr()
+    with open(str(log_file), 'r') as f:
+        log_content = f.read()
 
-    if filename:
-        try:
-            with open(filename, "r") as file:
-                log_content = file.read()
-                assert "test_func ok" in log_content
-        except FileNotFoundError:
-            pytest.fail(f"File '{filename}' not found.")
-    else:
-        assert "test_func ok" in captured.out
+    assert "test_function_file_success ok" in log_content
+
+    # Проверка вывода в файл ошибочную работу функции
+    test_function_file_error(1, 2)
+
+    with open(str(log_file), 'r') as f:
+        log_content = f.read()
+
+    assert "test_function_file_error error: ValueError. Inputs: (1, 2), {}" in log_content
+
+# def test_my_decorator():
+#     result = my_decorator(1, 2)
+#     assert result == 3
