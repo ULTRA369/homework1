72d73dcb0fd80baf397977b64e1acbcd96983dd6
Svetlana Linberg <ya.vgb@list.ru>
2024-09-02 20:20:10 +0300

    add file csv_excel_importer

---
+++ src/csv_excel_importer.py
@@ -0,0 +1,28 @@
+import csv
+import pandas as pd
+
+
+def csv_import(path_file):
+    """Функция принимает аргументом путь к файлу .csv и возвращает список словарей с транзакциями"""
+    transactions = []
+    try:
+        with open(path_file, 'r', encoding='utf-8') as csv_file:
+            reader = csv.reader(csv_file)
+            next(reader)    # закоментируй строку чтобы выdодилась первая строка тоже
+            for row in reader:
+                transactions.append(row)
+    except Exception as e:
+        print(f"Ошибка при считывании файла: {e}")
+    return transactions
+
+
+def excel_import(path_file):
+    """Функция принимает аргументом путь к файлу Excel и возвращает список словарей с транзакциями, в котором ключами
+     служат названия столбцов"""
+    try:
+        excel_data = pd.read_excel(path_file)
+        transactions = excel_data.to_dict(orient='records')
+        return transactions
+    except Exception as e:
+        print(f"Ошибка при считывании файла: {e}")
+        return []
