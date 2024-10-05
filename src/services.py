2fbb134be894c77fa82abf952b6a0bb01eecdcd3
Svetlana Linberg <ya.vgb@list.ru>
2024-10-05 03:27:01 +0300

    fix all project

--- src/services.py
+++ src/services.py
@@ -1,46 +1,36 @@
+
 import json
 import logging
-import os
-from datetime import datetime
+import re
 
-from config import LOGS_PATH
+from config import SERVICES_LOGS
 
-logger = logging.getLogger(f"{__name__}.py")
-handler = logging.FileHandler(os.path.join(LOGS_PATH, "logs.log"), mode="a")
-formatter = logging.Formatter("%(asctime)s %(levelname)s: %(name)s - %(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+logger = logging.getLogger(__file__)
 logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(SERVICES_LOGS, mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(funcName)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
-def cashback_categories(data: list[dict], year: str | int, month: str | int) -> json:
-    """
-    Считает размер кешбэка для каждой категории
-    Принимает данные с транзакциями в формате списка словарей, год, месяц для расчета.
-    Возвращает json-ответ со словарем категорий и размером кешбэка
-    """
-    logger.info("Start")
-    data_by_date = []
-    if year in range(1999, datetime.now().year):
-        for operation in data:
-            date = datetime.strptime(operation.get("Дата операции"), "%d.%m.%Y %H:%M:%S")
-            if date.year == year and date.month == month:
-                data_by_date.append(operation)
-    else:
-        logger.warning(f"Year is not in period 1999-{datetime.now().year}. Received all transactions")
-    categories = set()
-    for operation in data_by_date:
-        if operation.get("Категория").lower() not in ["пополнения", "переводы", "другое"]:
-            categories.add(operation.get("Категория"))
-    total_dict = dict()
-    for category in categories:
-        cashback_category = 0
-        for operation in data_by_date:
-            if operation.get("Категория") == category:
-                cashback_category += float(operation.get("Сумма операции с округлением")) / 100
-        total_dict[category] = round(cashback_category)
-    sort_list = sorted(total_dict.items(), key=lambda x: x[1], reverse=True)
-    sort_dict = dict(sort_list)
-    result = json.dumps(sort_dict, indent=4, ensure_ascii=False)
-    logger.info("Successful finish")
-    return result
+def df_to_dict(df):
+    return df.to_dict(orient="dict")
+
+
+def transfers_and_cash_grouped(data):
+    pattern = r"[А-Яа-яA-Za-z]+\s[А-Яа-яA-Za-z]\."
+    new_data = []
+    # Проходим по строкам словаря
+    for idx, category in data["Категория"].items():
+        if category == "Переводы":
+            description = data["Описание"][idx]  # Получаем описание этой строки
+
+            logger.info(f"Проверяем категорию: {category}, описание: {description}")
+
+            if re.search(pattern, description, re.IGNORECASE):  # Проверяем описание
+                new_data.append(description)
+                logger.info(f"Совпадение найдено: {description}")
+            else:
+                logger.error(f"Совпадение не найдено для описания: {description}")
+
+    return json.dumps(new_data, ensure_ascii=False)
